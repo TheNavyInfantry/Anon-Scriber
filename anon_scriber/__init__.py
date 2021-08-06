50e7e8b302e43d9a5c2f3fbc1ef957c2c9f6a895
TheNavyInfantry <nedimcanulusoy@gmail.com>
2021-08-07 00:32:39 +0300

    Restructured with Blueprint

--- anon_scriber/__init__.py
+++ anon_scriber/__init__.py
@@ -5,24 +5,37 @@ from flask_migrate import Migrate
 from datetime import datetime, timedelta
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from anon_scriber.config import Config
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+db = SQLAlchemy()
 
-app = Flask(__name__)
+migrate_db = Migrate(db)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = '88948ab134d6c6bab5b41493'
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=10)
+bcrypt = Bcrypt() # Used to hash stored passwords
 
-db = SQLAlchemy(app)
-
-Migrate(app, db)
-
-bcrypt = Bcrypt(app) # Used to hash stored passwords
-
-login_manager = LoginManager(app)
-login_manager.login_view = "login_page" # This line will redirect user when user tries to access unauthorized pages
+login_manager = LoginManager()
+login_manager.login_view = "users.login_page" # This line will redirect user when user tries to access unauthorized pages
 login_manager.login_message_category = 'warning' # This line will display an error message during unauthorized access
 
-from anon_scriber import routes
+
+
+
+def create_app(config_class=Config): # Do not take extension objects (eg. db, migrate) into this function
+    app = Flask(__name__)
+
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate_db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+
+    from anon_scriber.users.routes import users
+    from anon_scriber.posts.routes import posts
+    from anon_scriber.main.routes import main
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
